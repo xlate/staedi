5fd8256b1e0d096ac17551bfcdf0ff12ef8f9903
Michael Edgar <michael@xlate.io>
2020-03-04 21:33:27 -0500

    Add Sonar host to CI coverage script

--- ci_coverage.sh
+++ ci_coverage.sh
@@ -2,7 +2,7 @@
 
 if [[ $(bc -l <<< "$(java -version 2>&1 | awk -F '\"' '/version/ {print $2}' | awk -F'.' '{print $1"."$2}') >= 11") -eq 1 ]]; then
     export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo "pull-request-$TRAVIS_PULL_REQUEST"; fi)
-    mvn -Pcoverage -B verify sonar:sonar -Dsonar.projectKey=xlate_staedi -Dsonar.branch.name=$BRANCH
+    mvn -Pcoverage -B verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=xlate_staedi -Dsonar.branch.name=$BRANCH
 else
     echo "Not Java 11"
 fi
