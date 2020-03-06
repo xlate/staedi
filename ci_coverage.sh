0dbbbaad774ea483f3e374b544f2b030e1a79142
Michael Edgar <michael@xlate.io>
2020-03-06 06:58:02 -0500

    Disable Sonar analysis for PRs

--- ci_coverage.sh
+++ ci_coverage.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 if [[ $(bc -l <<< "$(java -version 2>&1 | awk -F '\"' '/version/ {print $2}' | awk -F'.' '{print $1"."$2}') >= 11") -eq 1 ]]; then
-    export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo "pull-request-$TRAVIS_PULL_REQUEST"; fi)
-    mvn -Pcoverage -B verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=xlate -Dsonar.projectKey=xlate_staedi -Dsonar.login=$SONAR_LOGIN -Dsonar.branch.name=$BRANCH
+    # export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo "pull-request-$TRAVIS_PULL_REQUEST"; fi)
+
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        mvn -Pcoverage -B verify sonar:sonar -Dsonar.projectKey=xlate_staedi
+    else
+        # Skip Sonar analysis for PRs (not supported)
+        mvn -Pcoverage -B verify
+    fi
 else
     echo "Not Java 11"
 fi
