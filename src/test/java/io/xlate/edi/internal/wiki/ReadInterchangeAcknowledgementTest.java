ccf6dcba829a84ce5cf50e68ef31a1045203d40b
Michael Edgar <michael@xlate.io>
2020-03-07 06:44:24 -0500

    Add test case for Wiki example

---
+++ src/test/java/io/xlate/edi/internal/wiki/ReadInterchangeAcknowledgementTest.java
@@ -0,0 +1,52 @@
+package io.xlate.edi.internal.wiki;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+import io.xlate.edi.stream.EDIInputFactory;
+import io.xlate.edi.stream.EDIStreamEvent;
+import io.xlate.edi.stream.EDIStreamReader;
+
+public class ReadInterchangeAcknowledgementTest {
+
+    @Test
+    public void testAcknowledgementSuccess() throws Exception {
+        assertTrue(isAcknowledgementSuccess());
+    }
+
+    private boolean isAcknowledgementSuccess() throws Exception {
+        EDIInputFactory factory = EDIInputFactory.newFactory();
+        String ta104 = null;
+        String ta105 = null;
+
+        //InputStream stream = new FileInputStream("x12_interchange_ack.txt");
+        try (InputStream stream = getClass().getResource("/wiki/x12_interchange_ack.txt").openStream()) {
+            EDIStreamReader reader = factory.createEDIStreamReader(stream);
+            EDIStreamEvent event;
+            String segment = null;
+
+            while (reader.hasNext()) {
+                event = reader.next();
+
+                if (event == EDIStreamEvent.START_SEGMENT) {
+                    segment = reader.getText();
+                } else if (event == EDIStreamEvent.ELEMENT_DATA) {
+                    if ("TA1".equals(segment)) {
+                        if (reader.getLocation().getElementPosition() == 4) {
+                            ta104 = reader.getText();
+                        } else if (reader.getLocation().getElementPosition() == 5) {
+                            ta105 = reader.getText();
+                        }
+                    }
+                }
+            }
+        }
+
+        return "A".equals(ta104) && "000".equals(ta105);
+    }
+
+}
---
+++ src/test/java/io/xlate/edi/internal/wiki/package-info.java
@@ -0,0 +1,1 @@
+package io.xlate.edi.internal.wiki;
