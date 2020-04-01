33affb4ff86beaa39ef6c78b39e01b7bd4f66c8b
Michael Edgar <michael@xlate.io>
2020-04-01 07:50:37 -0400

    Limit JavaDoc generation to Java 11

--- ci_coverage.sh
+++ ci_coverage.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ $(bc -l <<< "$(java -version 2>&1 | awk -F '\"' '/version/ {print $2}' | awk -F'.' '{print $1"."$2}') >= 11") -eq 1 ]]; then
+JAVA_VERSION=$(java -version 2>&1 | awk -F '\"' '/version/ {print $2}' | awk -F'.' '{print $1"."$2}')
+
+if [[ $(bc -l <<< "${JAVA_VERSION} >= 11") -eq 1 ]]; then
     # export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo "pull-request-$TRAVIS_PULL_REQUEST"; fi)
 
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
@@ -10,5 +12,5 @@ if [[ $(bc -l <<< "$(java -version 2>&1 | awk -F '\"' '/version/ {print $2}' | a
         mvn -Pcoverage -B verify
     fi
 else
-    echo "Not Java 11"
+    echo "Skipping coverage analysis for JAVA_VERSION: ${JAVA_VERSION}"
 fi
